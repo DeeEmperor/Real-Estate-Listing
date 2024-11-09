4c3769aceb0081ee47da16add7f382d1da1ed213
david adesina <deeemperor101@gmail.com>
2024-11-09 23:55:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("text.ejs", {});
+});
